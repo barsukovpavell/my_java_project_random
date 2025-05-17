bca96ecb1bf4a950677f6d22d89eb2eb6dbd4982
barsukovpavell <barsukovpavell@gmail.com>
2025-05-17 23:17:20 +0300

    commit_3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "Start";
-        System.out.println(str);
+        System.out.println(generateNamber());
+    }
+
+    public static int generateNamber() {
+        return (int) (Math.random() * 100);
     }
 }
