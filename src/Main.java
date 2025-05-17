c1d325111bf6c8acf6cec214235b83622a0cb0da
barsukovpavell <barsukovpavell@gmail.com>
2025-05-18 00:00:06 +0300

    commit_4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(generateNamber());
+        //System.out.println(generateNamber());
+        System.out.println(rollDice());
     }
 
     public static int generateNamber() {
         return (int) (Math.random() * 100);
+    }
+
+    public static int rollDice() {
+        int value;
+        value = (int) ((Math.random() * 6) + 1);
+        return value;
     }
 }
