a6e2697bee4c342fe40c07094fc459301ac2f984
barsukovpavell <barsukovpavell@gmail.com>
2025-05-20 00:04:44 +0300

    commit_6

--- src/MagicBall.java
+++ src/MagicBall.java
@@ -1,21 +1,35 @@
 import java.util.Random;
 
 public class MagicBall {
-    private static final String[] RESPONSE = {
+    private static final String A = "Бесспорно";
+    private static final String B = "Определённо да";
+    private static final String C = "Вероятнее всего";
+    private static final String D = "Хорошие перспективы";
+    private static final String F = "Спроси позже";
+    private static final String G = "Попробуй снова";
+    private static final String H = "Мой ответ — нет";
+    private static final String L = "Весьма сомнительно";
 
-            "Бесспорно",
-            "Определённо да",
-            "Вероятнее всего",
-            "Хорошие перспективы",
-            "Спроси позже",
-            "Попробуй снова",
-            "Мой ответ — нет",
-            "Весьма сомнительно"
-    };
-
-    public static String getPrediction() {
-        Random rand = new Random();
-        int namber = (int) rand.nextInt(RESPONSE.length);
-        return RESPONSE[namber];
+    public static String getPrediction(){
+        int rand = new Random().nextInt(8);
+        if(rand == 0){
+            return A;
+        } else if (rand == 1) {
+            return B;
+        } else if (rand == 2) {
+            return C;
+        } else if (rand == 3) {
+            return D;
+        } else if (rand == 4) {
+            return F;
+        } else if (rand == 5) {
+            return G;
+        }else if(rand == 6){
+            return H;
+        } else if (rand == 7) {
+            return L;
+        }else {
+            return null;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //System.out.println(generateNamber());
-        System.out.println(rollDice());
 
         System.out.println("Получится ли заработать на новую машину?");
         System.out.println(MagicBall.getPrediction());
     }
 
-    public static int generateNamber() {
-        return (int) (Math.random() * 100);
-    }
-
-    public static int rollDice() {
-        int value;
-        value = (int) ((Math.random() * 6) + 1);
-        return value;
-    }
 }
